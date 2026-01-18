6323428daadbb9cbdbd486299b4bb403c0d43071
QlioAmanda <qlioamanda@gmail.com>
2026-01-18 22:47:32 +0700

    update build.bat & readme

--- build.bat
+++ build.bat
@@ -1,9 +1,30 @@
 @echo off
-echo === Compile Java ===
+title KRSApp Builder
+cls
+
+echo ==============================================
+echo        SISTEM BUILD & RUN KRS APP
+echo ==============================================
+
+REM 1. Pastikan folder bin tersedia
+echo [+] Memeriksa direktori output...
+if not exist bin mkdir bin
+
+REM 2. Compile source code
+echo [+] Melakukan Kompilasi Java...
 javac -d bin src/id/ac/polban/model/*.java src/id/ac/polban/service/*.java
 
-echo === Buat JAR ===
+REM 3. Packaging ke JAR
+echo [+] Membuat file JAR...
 jar cfm KRSApp.jar manifest.txt -C bin .
 
-echo === Jalankan Program ===
+echo ==============================================
+echo        BUILD SUKSES - MENJALANKAN
+echo ==============================================
+echo.
+
+REM 4. Jalankan aplikasi
 java -jar KRSApp.jar
+
+echo.
+pause
